50f201f88878ebd958b8476e760c0862e1883bd5
Ross-Esmond <r.esmond@protonmail.com>
2025-11-23 12:38:32 -0600

    feat: added eslint and prettier

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,7 @@
 export default {
-	input: 'src/index.ts',
-	output: {
-		file: 'build/bundle.js',
-		format: 'cjs',
-	}
+  input: "src/index.ts",
+  output: {
+    file: "build/bundle.js",
+    format: "cjs",
+  },
 };
